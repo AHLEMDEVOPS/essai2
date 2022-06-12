5d63d2d318986e80f3a7c6d5233817cce152e3cc
ahlem <ferchichi_ahlem@hotmail.fr>
2022-06-12 21:29:22 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+app.get ('/parkings',(req,res)=>{
+    res.send("liste des parkings")
+})
+app.listen(8080,()=>{
+    console.log('serveur à l écoute')
+})
